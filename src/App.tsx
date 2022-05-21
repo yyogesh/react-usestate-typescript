91ab4f7fdec054fbdb2c04a8c65ce266255c6ad0
yyogesh <yogeshggn1@gmail.com>
2022-05-21 12:10:22 +0530

    useState Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
+// const Heading = ({ title }: { title: string }) => <h2>{title}</h2>
+
+const Heading: React.FC<{ title: string }> = ({ title }) => <h2>{title}</h2>
+
+// const Box = ({ children }: { children: React.ReactNode }) => <div className="box">{children}</div>
+
+const Box: React.FunctionComponent<{ children: React.ReactNode }> = ({ children }) => <div className="box">{children}</div>
+
+
+interface ListItem {
+  items: string[];
+  itemClick?: (item: string) => void;
+}
+
+const List: React.FC<ListItem> = ({ items, itemClick }) => {
+  return <ul>
+    {
+      items.map((item, index) => <li onClick={() => itemClick?.(item)} key={index}>{item}</li>)
+    }
+  </ul>
+}
+
+interface PersonProps {
+  name: string;
+  age: number;
+  hobbies: Array<string>;
+  isCool: boolean;
+}
+
 function App() {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  // Boolean type
+  const [isCool] = useState<boolean>(true);
+  // String type
+  const [name] = React.useState<string>('Ruben');
+
+  // Number type
+  const [age] = React.useState<number>(28);
+
+  // Null or undefined
+  const [random] = React.useState<null | undefined>();
+
+  // Array of string 
+  const [hobbies] = React.useState<Array<string>>(['soccer', 'cooking', 'code']);
+  // Custom interface
+  const [person] = React.useState<PersonProps>({
+    isCool,
+    name,
+    age,
+    hobbies
+  });
+
+  const itemClick = (item: string) => {
+    console.log(item);
+  }
+  const addNewTodo = () => {
+    console.log(inputEl.current?.value);
+  }
+
+  useEffect(() => {
+    document.title = "Ruben's page";
+  }, [])
+
+  console.log('render');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Heading title="Todos" />
+      <Box>
+        this is one of todos app
+        <List items={['study', 'reading book']} itemClick={itemClick} />
+      </Box>
+      <div>
+        <input ref={inputEl} type={'text'} />
+        <button onClick={addNewTodo}>Add</button>
+      </div>
+    </div >
   );
 }
 
